e3b298a7fc6d184d6438ea90afd07ba093d7d669
anderson <andinho@gmail.com>
2022-05-25 03:51:56 -0300

    Initial commit

---
+++ src/one/digitalinovation/basecamp/Main.java
@@ -0,0 +1,53 @@
+package one.digitalinovation.basecamp;
+
+public class Main {
+
+    public static void main(String [] arqs) {
+        int i;
+        //int i;
+        int I;
+        //int 1;
+        int _1a;
+        int $ab;
+
+        i =5;
+        I =10;
+        _1a = 20;
+        $ab = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678nd;
+        //int asrn246 78nd;
+        int asrn2$4678_nd = 10;
+        //int asrn2$46%78_nd =10;
+
+        asrn24678nd = 100;
+        asrn2$4678_nd = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        // int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($ab);
+
+        System.out.println(j);
+        System.out.println(asrn24678nd);
+        System.out.println(asrn2$4678_nd);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+
+    }
+
+}
